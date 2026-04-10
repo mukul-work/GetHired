d8f169eb963bbf321fc029d13f86ed953573c17a
Mukul Kashyap <mukulkashyap.business@gmail.com>
2026-04-10 16:26:16 +0530

    feat: add search and tag filter to blogs page

--- client/src/pages/blogs/Blogs.jsx
+++ client/src/pages/blogs/Blogs.jsx
@@ -1,15 +1,38 @@
-import blogs from "../../data/blogs";
-import BlogCard from "../../components/blogs/BlogCard";
+import { useState } from 'react';
+import blogs from '../../data/blogs.js';
+import BlogCard from '../../components/blogs/BlogCard.jsx';
 
-export default function Blogs() {
-    return (
-        <div>
-            <h1>Blogs Page</h1>
-            <div>
-                {blogs.map(blog => (
-                    <BlogCard key={blog.id} blog={blog}/>
-                ))}
-            </div>
-        </div>
-    )
+const allTags = [...new Set(blogs.flatMap(blog => blog.tags))];
+
+function Blogs() {
+  const [search, setSearch] = useState('');
+  const [selectedTag, setSelectedTag] = useState('');
+
+  const filtered = blogs.filter(blog => {
+    const matchesSearch = blog.title.toLowerCase().includes(search.toLowerCase());
+    const matchesTag = selectedTag === '' || blog.tags.includes(selectedTag);
+    return matchesSearch && matchesTag;
+  });
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Search blogs..."
+        value={search}
+        onChange={e => setSearch(e.target.value)}
+      />
+      <div>
+        <button onClick={() => setSelectedTag('')}>All</button>
+        {allTags.map(tag => (
+          <button key={tag} onClick={() => setSelectedTag(tag)}>{tag}</button>
+        ))}
+      </div>
+      {filtered.map(blog => (
+        <BlogCard key={blog.id} blog={blog} />
+      ))}
+    </div>
+  );
 }
+
+export default Blogs;
