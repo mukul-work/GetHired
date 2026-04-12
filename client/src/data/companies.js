f7b004a9b0ec30d67c4875ba37819082dc24d2d8
MahanSinghRajput <mahan.2428cse1312@kiet.edu>
2026-04-12 22:35:30 +0530

    a

---
+++ client/src/data/companies.js
@@ -0,0 +1,40 @@
+export const companies = [
+  {
+    id: 1,
+    name: "Google",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/2/2f/Google_2015_logo.svg",
+    studentsPlaced: 25,
+    offers: 30,
+    domains: ["SDE", "ML"],
+    locations: ["Bangalore", "Hyderabad"],
+    avgPackage: "28 LPA",
+    highestPackage: "60 LPA",
+    topics: ["DSA", "System Design", "OS"],
+    difficulty: "Hard",
+
+    history: [
+      { year: 2024, students: 25, offers: 30, avg: "28 LPA", highest: "60 LPA" },
+      { year: 2023, students: 20, offers: 26, avg: "25 LPA", highest: "55 LPA" },
+      { year: 2022, students: 18, offers: 22, avg: "22 LPA", highest: "50 LPA" }
+    ]
+  },
+  {
+    id: 2,
+    name: "Amazon",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/a/a9/Amazon_logo.svg",
+    studentsPlaced: 40,
+    offers: 50,
+    domains: ["SDE"],
+    locations: ["Hyderabad", "Chennai"],
+    avgPackage: "20 LPA",
+    highestPackage: "45 LPA",
+    topics: ["DSA", "LLD"],
+    difficulty: "Medium",
+
+    history: [
+      { year: 2024, students: 40, offers: 50, avg: "20 LPA", highest: "45 LPA" },
+      { year: 2023, students: 35, offers: 42, avg: "18 LPA", highest: "40 LPA" },
+      { year: 2022, students: 30, offers: 38, avg: "16 LPA", highest: "35 LPA" }
+    ]
+  }
+]
